feb6e7a1bb589b48a5b7ab92e2425f6fa3879073
Tony Li <jintli@redhat.com>
2016-06-05 05:54:31 -0400

    add IFNAME for network assingment

--- k2c-rhel-repos/build.sh
+++ k2c-rhel-repos/build.sh
@@ -2,10 +2,22 @@ docker rm -f $(basename `pwd`)
 #docker rmi -f $(basename `pwd`)
 docker build -t $(basename `pwd`) .
 docker images | grep "<none>" | awk '{print $3}' | xargs docker rmi -f 
+
+export MY_IFNAME=atomic1
 export MY_IP=192.170.0.229
 export MY_PREFIX=16
 export MY_GATEWAY=192.170.0.1
 export YUM_REPO_PATH=/home/repos
 atomic install k2c-rhel-repos
 /usr/bin/systemctl restart k2c-rhel-repos.service
-docker attach k2c-rhel-repos
+
+docker rm -f k2c-rhel-repos-int
+export MY_IFNAME=atomic2
+export MY_IP=172.16.0.229
+export MY_PREFIX=16
+export MY_GATEWAY=172.16.0.254
+export YUM_REPO_PATH=/home/repos
+atomic install -n k2c-rhel-repos-int k2c-rhel-repos
+/usr/bin/systemctl restart k2c-rhel-repos-int.service
+
+docker attach k2c-rhel-repos-int
